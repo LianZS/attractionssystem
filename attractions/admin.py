a274ef5170b9c58eed07e53a36ceae1497524dfd
lianzongsheng <2214057556@qq.com>
2020-02-10 23:54:14 +0800

    管理员界面

--- attractions/admin.py
+++ attractions/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import AttractionInfo, CityInfo, ProvinceInfo
+
 
 # Register your models here.
+@admin.register(AttractionInfo)
+class AttractionInfoAdmin(admin.ModelAdmin):
+    list_per_page = 10
+
+
+@admin.register(CityInfo)
+class CityInfoAdmin(admin.ModelAdmin):
+    list_per_page = 10
+
+
+@admin.register(ProvinceInfo)
+class ProvinceModelAdmin(admin.ModelAdmin):
+    list_per_page = 10
