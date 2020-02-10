a651242ebe02d1a15098b9509b7798e2a5967335
lianzongsheng <2214057556@qq.com>
2020-02-10 16:01:22 +0800

    media配置

--- attractionssystem/settings.py
+++ attractionssystem/settings.py
@@ -62,6 +62,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
+
             ],
         },
     },
@@ -115,3 +117,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), os.path.join(BASE_DIR, "attractions/static"), ]
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+MEDIA_URL = '/media/'
