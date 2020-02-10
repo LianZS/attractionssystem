dc2b846e2dcd9fd1c33ff6c1b9711432fb3d0e1c
lianzongsheng <2214057556@qq.com>
2020-02-10 16:02:18 +0800

    静态文件访问设置

--- attractions/urls.py
+++ attractions/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path,include
-from .views import first_page_view
-urlpatterns = [
-    path('',first_page_view),
+from django.urls import path
+from django.conf import settings
 
-]
+from .views import ProvinceNavigation
+from django.conf.urls.static import static
+
+app_name = 'attractions'
+urlpatterns = [
+                  path('navigation', ProvinceNavigation.as_view(), name='province_list'),
+
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
+              static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
