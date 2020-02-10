faedf9a882170d370948374f71abf0073c2709e2
lianzongsheng <2214057556@qq.com>
2020-02-10 16:01:52 +0800

    attractions命名空间设置

--- attractionssystem/urls.py
+++ attractionssystem/urls.py
@@ -18,6 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('attractions.urls')),
+    path('',include('attractions.urls',namespace='attractions')),
 
 ]
